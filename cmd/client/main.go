c61414046cffc6bc77766e4268eb2c8180e40a07
Kriss-Kolak <krzysiukolak@gmail.com>
2025-12-06 17:24:57 +0100

    client side message queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	const connString = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer ch.Close()
+
 	fmt.Println("Starting Peril client...")
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,9 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/Kriss-Kolak/learn-pub-sub-starter/internal/pubsub"
+	"github.com/Kriss-Kolak/learn-pub-sub-starter/internal/routing"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -12,13 +15,18 @@ func main() {
 	const connString = "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(connString)
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
 	ch, err := conn.Channel()
 	if err != nil {
+		fmt.Println(err)
 		return
 	}
+	defer ch.Close()
+
+	pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
 
 	fmt.Println("Successfully connected to rabbitmq")
 	fmt.Println("Starting Peril server...")
